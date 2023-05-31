820f2f99c62de151e1629c67c9eb1790ca3bf23c
Rifaat-SaroutB09 <rifaat.sarout2@gmail.com>
2023-05-31 14:01:20 +0300

    new update

---
+++ src/Components/footer/Footer.jsx
@@ -0,0 +1,72 @@
+import React from "react";
+import "./Footer.css";
+
+function Footer() {
+  return (
+    <div className="footer">
+      <div className="container">
+        <div className="top">
+          <div className="item">
+            <h2>Categories</h2>
+            <span>Graphics & Design</span>
+            <span>Digital Marketing</span>
+            <span>Writing & Translation</span>
+            <span>Video & Animation</span>
+            <span>Music & Audio</span>
+            <span>Programming & Tech</span>
+            <span>Data</span>
+            <span>Business</span>
+            <span>Lifestyle</span>
+            <span>Photography</span>
+            <span>Sitemap</span>
+          </div>
+          <div className="item">
+            <h2>About</h2>
+            <span>Press & News</span>
+            <span>Partnerships</span>
+            <span>Privacy Policy</span>
+            <span>Terms of Service</span>
+            <span>Intellectual Property Claims</span>
+            <span>Investor Relations</span>
+            <span>Contact Sales</span>
+          </div>
+          <div className="item">
+            <h2>Support</h2>
+            <span>Help & Support</span>
+            <span>Trust & Safety</span>
+            <span>Selling on Fiverr</span>
+            <span>Buying on Fiverr</span>
+          </div>
+          
+        </div>
+        <hr />
+        <div className="bottom">
+          <div className="left">
+            <h2>Refferr</h2>
+            <span>© Refferr International Ltd. 2023</span>
+          </div>
+          <div className="right">
+            <div className="social">
+              <img src="/img/twitter.png" alt="" />
+              <img src="/img/facebook.png" alt="" />
+              <img src="/img/linkedin.png" alt="" />
+              <img src="/img/pinterest.png" alt="" />
+              <img src="/img/instagram.png" alt="" />
+            </div>
+            <div className="link">
+              <img src="/img/language.png" alt="" />
+              <span>English</span>
+            </div>
+            <div className="link">
+              <img src="/img/coin.png" alt="" />
+              <span>USD</span>
+            </div>
+            <img src="/img/accessibility.png" alt="" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
